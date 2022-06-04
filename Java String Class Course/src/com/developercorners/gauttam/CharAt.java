86f6d5c6ac21c4327c3cfb275d5e6592a284a5ed
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2022-06-05 04:10:23 +0530

    Add files via upload

---
+++ Java String Class Course/src/com/developercorners/gauttam/CharAt.java
@@ -0,0 +1,23 @@
+package com.developercorners.gauttam;
+
+public class CharAt {
+
+	public static void main(String[] args) {
+		
+		String name="DeveloperCorners";
+		
+		String s1=name.substring(2,4);
+		String s2=name.substring(5);
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		char ch=name.charAt(0);
+		
+		int len=name.length();
+		
+		
+		System.out.println(len);
+//		System.out.println(ch);
+	}
+
+}
